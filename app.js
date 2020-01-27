12abdddf384bf2cea4e0e4986ab98f174cf66eff
Lucas Livchits <livchitsl@gmail.com>
2020-01-27 10:48:54 -0300

    Add error handling middleware

--- app.js
+++ app.js
@@ -15,4 +15,16 @@ app.use(express.json());
 
 app.use('/api', routes);
 
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  return res.json({
+    message: err.message,
+    /*
+     if we're in development mode, include stack trace (full error object)
+     otherwise, it's an empty object so the user doesn't see all of that
+    */
+    error: app.get('env') === 'development' ? err : {}
+  });
+});
+
 app.listen(PORT, () => console.log(`Server listening on http://${HOSTNAME}:${PORT}`));
