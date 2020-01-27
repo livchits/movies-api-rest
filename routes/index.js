3f9d2b8f8d83a43c01fa25d25bdf3f9126367455
Lucas Livchits <livchitsl@gmail.com>
2020-01-27 11:04:48 -0300

    Add post router for /movie endpoint

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,18 @@ const movies = require('../movies');
 const { checkSchema, validationResult } = require('express-validator');
 const { postValidation, putValidation } = require('../validations');
 
-router.route('/movies').get((req, res) => {
-  res.json(movies.list);
-});
+router
+  .route('/movies')
+  .get((req, res) => {
+    res.json(movies.list);
+  })
+  .post(checkSchema(postValidation), (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const err = new Error('400 - Bad Request');
+      //err.status = 400;
+      return next(err);
+    }
+
+    return res.json(movies.addMovie(req.body));
+  });
