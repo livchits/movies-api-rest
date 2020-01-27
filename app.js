cc21b97dba8eb8fe46959b22684ced0300a9727c
Lucas Livchits <livchitsl@gmail.com>
2020-01-27 10:45:08 -0300

    Initial server setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const routes = require('./routes');
+const morgan = require('morgan');
+
+dotenv.config();
+
+const { PORT = 8001, HOSTNAME } = process.env;
+
+const app = express();
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server listening on http://${HOSTNAME}:${PORT}`));
