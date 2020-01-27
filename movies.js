4116e35d80e5240d2e7dc9f72c0689c1bba5558d
Lucas Livchits <livchitsl@gmail.com>
2020-01-27 10:53:26 -0300

    Add movies.js module

---
+++ movies.js
@@ -0,0 +1,16 @@
+const movies = {
+  listOfMovies: [
+    {
+      id: 1,
+      title: 'Back to the Future',
+      year: 1985,
+      genre: 'Science Fiction'
+    },
+    {
+      id: 2,
+      title: 'Back to the Future II',
+      year: 1987,
+      genre: 'Science Fiction'
+    }
+  ]
+};
