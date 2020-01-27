d8485087154c20c59876d8d69320858ff968a715
Lucas Livchits <livchitsl@gmail.com>
2020-01-27 10:58:06 -0300

    Add get router for /movie endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const movies = require('../movies');
+
+router.route('/movies').get((req, res) => {
+  res.json(movies.list);
+});
