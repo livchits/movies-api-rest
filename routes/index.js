d79066ba5230febf8eae781ec15b03f88fb60ab1
Lucas Livchits <livchitsl@gmail.com>
2020-01-27 11:01:56 -0300

    Import express-validator and schemas

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const movies = require('../movies');
+const { checkSchema, validationResult } = require('express-validator');
+const { postValidation, putValidation } = require('../validations');
 
 router.route('/movies').get((req, res) => {
   res.json(movies.list);
