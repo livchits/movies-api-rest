93102d738649137a627bb21baac9da4dbb688e3e
Lucas Livchits <livchitsl@gmail.com>
2020-01-30 11:04:28 -0300

    Empties list of movies for production

--- movies.js
+++ movies.js
@@ -1,18 +1,5 @@
 const movies = {
-  listOfMovies: [
-    {
-      id: 1,
-      title: 'Back to the Future',
-      year: 1985,
-      genre: 'Science Fiction'
-    },
-    {
-      id: 2,
-      title: 'Back to the Future II',
-      year: 1987,
-      genre: 'Science Fiction'
-    }
-  ],
+  listOfMovies: [],
   get list() {
     return this.listOfMovies;
   },
